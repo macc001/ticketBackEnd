e29a4d2129baef8830a6a4e700fd1c6a5bab98f0
miguel angel cayo carvajal <miguelcayocarvajal37@gmail.com>
2019-12-14 03:22:06 -0400

    add nuevas funciones para admin

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -129,4 +129,98 @@ async function mostrar(req, res) {
   connection.end();
 }
 
-module.exports = { validar_ci, insert_ticket, login, mostrar };
+async function regProfe(req, res) {
+  var { item, ci, nombre, sie, colegio, distrito } = req.body;
+  var connection = mysql.createConnection(globalDB);
+  connection.connect();
+  if (item) {
+    if (ci) {
+      if (nombre) {
+        if (sie) {
+          if (colegio) {
+            if (distrito) {
+              const queryy = "CALL insertar_profesor(?,?,?,?,?,?);";
+              await connection.query(
+                queryy,
+                [item, ci, nombre, sie, colegio, distrito],
+                (err, rows, fields) => {
+                  // si no hay fila insertada mostrar mensaje de error
+                  if (!err) {
+                    res.json(rows[0]);
+                  } else {
+                    res.status(200).send({
+                      err
+                    });
+                  }
+                }
+              );
+            } else {
+              res.status(200).send({
+                messagge: "complete el campo distrito"
+              });
+            }
+          } else {
+            res.status(200).send({
+              messagge: "complete el campo colegio"
+            });
+          }
+        } else {
+          res.status(200).send({
+            messagge: "complete el campo sie"
+          });
+        }
+      } else {
+        res.status(200).send({
+          messagge: "complete el campo nombre"
+        });
+      }
+    } else {
+      res.status(200).send({
+        messagge: "complete el campo ci"
+      });
+    }
+  } else {
+    res.status(200).send({
+      messagge: "complete el campo item"
+    });
+  }
+  connection.end();
+}
+
+async function entregarProf(req, res) {
+  var { ci, observacion } = req.body;
+  var connection = mysql.createConnection(globalDB);
+  connection.connect();
+  if (ci) {
+    if (observacion) {
+      const queryy = "CALL Actualizar_Observacion(?,?);";
+      await connection.query(queryy, [ci, observacion], (err, rows, fields) => {
+        if (!err) {
+          res.json(rows[0]);
+        } else {
+          res.status(200).send({
+            err
+          });
+        }
+      });
+    } else {
+      res.status(200).send({
+        messagge: "complete el campo observacion"
+      });
+    }
+  } else {
+    res.status(200).send({
+      messagge: "complete el campo ci"
+    });
+  }
+  connection.end();
+}
+
+module.exports = {
+  validar_ci,
+  insert_ticket,
+  login,
+  mostrar,
+  regProfe,
+  entregarProf
+};
--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -5,7 +5,11 @@ const usuario = require("../controllers/user.controllers");
 
 router.post("/buscar", usuario.validar_ci);
 router.post("/insert", usuario.insert_ticket);
+// rol 1 admin - rol 0 usuario normal
 router.post("/login", usuario.login);
 router.post("/mostrar", usuario.mostrar);
+// en proceso v2
+router.post("/registrar-profesor", usuario.regProfe);
+router.post("/entregar-profesor", usuario.entregarProf);
 
 module.exports = router;
